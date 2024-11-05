ad1e6e9abb0bf3d529f9f8506c8689f9dfa4cd1d
fcarlos2k@gmail.com <fabricio.araujo@corregedoria.df>
2024-11-05 15:18:42 -0300

    Comecando o projeto de mensagens bancarias

---
+++ NotificacaoBanco/Entities/Notificacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificacaoBanco.Entities
+{
+    internal class Notificacao
+    {
+
+        public string notificacao { get; set; }
+        public string tipo { get; set; }
+
+        public Notificacao()
+        {
+        }
+
+        public Notificacao(string notificacao, string tipo)
+        {
+            this.notificacao = notificacao;
+            this.tipo = tipo;
+        }
+    }
+}
--- NotificacaoBanco/obj/Debug/net8.0/NotificacaoBanco.AssemblyInfo.cs
+++ NotificacaoBanco/obj/Debug/net8.0/NotificacaoBanco.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDePedidos")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NotificacaoBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74cdd51ae8f8a82ea1aaa7e4d7d0d2b2c122637c")]
-[assembly: System.Reflection.AssemblyProductAttribute("GestaoDePedidos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("GestaoDePedidos")]
+[assembly: System.Reflection.AssemblyProductAttribute("NotificacaoBanco")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NotificacaoBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
