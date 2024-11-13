95b4849d4c599fe521b7d8f02cbff643e9ee285c
fcarlos2k <fcarlos2k@gmail.com>
2024-11-13 13:52:15 -0300

    enviando arquivo da PrimeiraBranch com conflito para teste

--- SuporteTecnico/Program.cs
+++ SuporteTecnico/Program.cs
@@ -7,12 +7,15 @@ namespace SuporteTenico // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Teste de alteração pra novo commit");
+<<<<<<< HEAD
             Console.WriteLine("Teste de alteração pra nova branch");
             Console.WriteLine("Teste de alteração na branch main para testar na primeira branch");
             Console.WriteLine("Teste de alteração na branch main para testar na primeira branch");
             Console.WriteLine("Teste de alteração na branch main para testar na primeira branch");
 
 
+=======
+>>>>>>> parent of 1f8b1da (commit na nova branch)
         }
     }
 }
